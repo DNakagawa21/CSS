e248f1834c8e17bf5d79de15a418b6edfa011eb0
DNakagawa21 <103960269+DNakagawa21@users.noreply.github.com>
2022-04-19 14:20:33 -0700

    finished

---
+++ algos.js
@@ -0,0 +1,14 @@
+// Variables
+
+// Boxes that store information
+// we use keywords to make new variables 
+// we can only put data inside of variables, not expressions
+var box1 = 42;
+let box2 = 2 + 2; // this is an expression, no a piece of data
+const box3 = "hello there";
+
+// Naming Conventions
+
+var isThisAVariable = "Hello"
+console.log(isThisAVariable)
+
